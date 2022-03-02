ba304aa7e6bd4f60f3eb3e2a517c087c481d4957
pascal910107 <95022717+pascal910107@users.noreply.github.com>
2022-03-02 18:34:19 +0800

    Add files via upload

---
+++ ilearn 3.0/my.js
@@ -0,0 +1,8 @@
+// var bannerads = document.querySelector('.bannerads');
+// var prev = document.querySelector('#prev');
+// var next = document.querySelector('#next');
+// bannerads.onmouseover = function(){
+//     prev.className = 'prev';
+//     next.className = 'next';
+// }
+
