8b53ff0da6fe31eac6552faf407ce44176337b14
Pascal <a306090603@gmail.com>
2022-03-04 21:47:13 +0800

    .

--- my.js
+++
@@ -1,8 +0,0 @@
-// var bannerads = document.querySelector('.bannerads');
-// var prev = document.querySelector('#prev');
-// var next = document.querySelector('#next');
-// bannerads.onmouseover = function(){
-//     prev.className = 'prev';
-//     next.className = 'next';
-// }
-
